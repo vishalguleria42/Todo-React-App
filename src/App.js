a29e1f7519ca3a6e22c019ac4b830c8db86c234a
Vishal Guleria <vishalguleria9816@gmail.com>
2023-12-02 19:53:17 +0530

    Initial layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import TodoForm from "./TodoForm";
+import TodoList from "./TodoList";
+import "./App.css";
 
-function App() {
+const LOCAL_STORAGE_KEY = "react-todo-list-task";
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
+
+  const addTodo = (todo) => {
+    if (!todo.text) return;
+    setTodos([todo, ...todos]);
+  };
+
+  const removeTodo = (id) => {
+    const removeArray = todos.filter((todo) => todo.id !== id);
+    setTodos(removeArray);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text) return;
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="main-container">
+      <h1 className="todolist-header">TODO List</h1>
+      <TodoForm onSubmit={addTodo} />
+      <TodoList
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </section>
   );
-}
+};
 
 export default App;
